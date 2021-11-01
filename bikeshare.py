fb039a24a7d624b48544c5c4c0b4eeb4a2d947a1
David Lippert <lippertd88@gmail.com>
2021-11-01 23:09:19 +0200

    changed weekday_name to day_name()

--- bikeshare.py
+++ bikeshare.py
@@ -64,7 +64,7 @@ def load_data(city, month, day):
 
     df['month'] = df['Start Time'].dt.month
 
-    df['day_of_week'] = df['Start Time'].dt.weekday_name
+    df['day_of_week'] = df['Start Time'].dt.day_name()
 
     if month != 'all':
         months = ['january','february','march','april','may','june']
